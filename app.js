2d56622df7649d9f50476972deaf97bfb4d5df4e
Rafael Segarra <rafaelandres54@gmail.com>
2020-11-19 16:07:13 -0500

    second update

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const layout = require('./views/layout');
+const { db, User, Page } = require('./models');
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -9,10 +15,28 @@ app.use(express.urlencoded({
     extended: false
 }))
 
+// (async()=> {
+//     await User.sync
+//     await Page.sync
+// })()
+
+
 app.get('/', (req, res) =>{
     res.send(layout(' '));
 })
 
-app.listen(3000, () =>{
-    console.log('app listening on 3000')
-})
+const PORT = 3000;
+
+
+const init = async () => {
+    await db.sync();
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}!`);
+    });
+  }
+  
+  init();
+
+// app.listen(3000, () =>{
+//     console.log('app listening on 3000')
+// })
